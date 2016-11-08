d96060b7f1dea059bc27836b18861a7e98e4da70
CLofton <lofton.chris@gmail.com>
2016-11-08 16:29:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,58 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Instructor Training</title>
+</head>
+<body>
+
+<h1>Instructor Training</h1>
+
+  <h2>Planning</h2>
+    <ul>
+        <li>Knowledge of Students<li>
+        <li>Lesson Cycle</li>
+        <li>Contribute to Curriculum</li>
+    </ul>
+
+  <h2>Teaching<h2>
+    <ul>
+        <li>Student Engagment<li>
+        <li>Adult Learning Strategies</li>
+        <li>Classroom Structure and Layout</li>
+    </ul>
+
+    <h2>Professionalism</h2>
+      <ul>
+        <li>Economic Collaboratives</li>
+        <li>Continued Learning</li>
+        <li>Ethics and Sensitivity</li>
+        <li>On-Boarding Paperwork (link to docusign coming soon)</li>
+        <li>Operational Responsibilities</li>
+      </ul>
+
+    <h2>Helpful Links<h2>
+      <ul>
+        <li>Campus Manager</li>
+          <ul>
+            <li>Attendance</li>
+            <li>Grades</li>
+            <li>Screencasts</li>
+          </ul>
+        <li>Github</li>
+          <ul>
+            <li>Getting students started with the workbook</li>
+              <ul>
+                <li>Desktop App</li>
+                <li>Terminal</li>
+              </ul>
+          </ul>
+        <li>Gitbooks</li>
+        <li>Rocket Chat</li>
+        <li>CircleCi</li>
+      </ul>
+
+
+
+
+</body>
+</html>
