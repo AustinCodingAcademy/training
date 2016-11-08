f0a7e05a4a3e8d2b46b7f3af5c0a2f1e803e9ea5
Chris Lofton <lofton.chris@gmail.com>
2016-11-08 17:55:44 -0600

    updated

--- index.php
+++ index.php
@@ -2,62 +2,63 @@
 <html>
 <head>
 <title>Instructor Training</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+<pnk rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
 <h1>Instructor Training</h1>
 
-  <h2>Planning</h2>
-    <ul>
-        <li><a href="#knowledge-of-students">Knowledge of Students</a></li>
-        <li>Lesson Cycle</li>
-        <li>Contribute to Curriculum</li>
-    </ul>
+  <div class="center">
 
-  <h2>Teaching</h2>
-    <ul>
-        <li>Student Engagment</li>
-        <li>Adult Learning Strategies</li>
-        <li>Classroom Structure and Layout</li>
-    </ul>
+    <h2>Planning</h2>
+          <p><a href="#knowledge-of-students">Knowledge of Students</a></p>
+          <p>Lesson Cycle</p>
+          <p>Contribute to Curriculum</p>
 
-    <h2>Professionalism</h2>
-      <ul>
-        <li>Economic Collaboratives</li>
-        <li>Continued Learning</li>
-        <li>Ethics and Sensitivity</li>
-        <li>On-Boarding Paperwork (link to docusign coming soon)</li>
-        <li>Operational Responsibilities</li>
-      </ul>
+    <h2>Teaching</h2>
+          <p>Student Engagment</p>
+          <p>Adult Learning Strategies</p>
+          <p>Classroom Structure and Layout</p>
 
-    <h2>Helpful Links</h2>
-      <ul>
-        <li>Campus Manager</li>
-          <ul>
-            <li>Attendance</li>
-            <li>Grades</li>
-            <li>Screencasts</li>
-          </ul>
-        <li>Github</li>
-          <ul>
-            <li>Getting students started with the workbook</li>
-              <ul>
-                <li>Desktop App</li>
-                <li>Terminal</li>
-              </ul>
-          </ul>
-        <li>Gitbooks</li>
-        <li>Rocket Chat</li>
-        <li>CircleCi</li>
-      </ul>
+      <h2>Professionapsm</h2>
+          <p>Economic Collaboratives</p>
+          <p>Continued Learning</p>
+          <p>Ethics and Sensitivity</p>
+          <p>On-Boarding Paperwork (pnk to docusign coming soon)</p>
+          <p>Operational Responsibipties</p>
+      <div>
+
+    <div class="left">
+
+      <h2>Helpful Links</h2>
+        <ul>
+          <p>Campus Manager</p>
+            <ul>
+              <p>Attendance</p>
+              <p>Grades</p>
+              <p>Screencasts</p>
+            </ul>
+          <p>Github</p>
+            <ul>
+              <p>Getting students started with the workbook</p>
+                <ul>
+                  <p>Desktop App</p>
+                  <p>Terminal</p>
+                </ul>
+            </ul>
+          <p>Gitbooks</p>
+          <p>Rocket Chat</p>
+          <p>CircleCi</p>
+        </ul>
+
+      </div>
 
     <div id="knowledge-of-students">
       <h2>Knowledge of Students</h2>
         <ul>
-          <li>Knowledge of Students Video:Need to make</li>
-          <li>Read this <a href="https://mindstepsinc.com/2012/09/activating-prior-knowledge/">Knowledge of Students Article</a></li>
-          <li>Complete this assessment:</li>
+          <p>Knowledge of Students Video:Need to make</p>
+          <p>Read this <a href="https://mindstepsinc.com/2012/09/activating-prior-knowledge/">Knowledge of Students Article</a></p>
+          <p>Complete this assessment:</p>
         </ul>
           <iframe src="https://docs.google.com/a/austincodingacademy.com/forms/d/e/1FAIpQLSe4tujNP3wskr3ac2ZM4W0oGSH5x9PHODIHeqGGTRGCG7gwmA/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
     </div>
