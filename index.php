aa80d6166eb51465f27830dff989fe6a71a7d803
Chris Lofton <lofton.chris@gmail.com>
2016-11-08 18:46:15 -0600

    updated

--- index.php
+++ index.php
@@ -12,56 +12,81 @@
 
     <h2>Planning</h2>
           <p><a href="#knowledge-of-students">Knowledge of Students</a></p>
-          <p>Lesson Cycle</p>
-          <p>Contribute to Curriculum</p>
+          <p><a href="#lesson-cycle">Lesson Cycle</a></p>
+          <p><a href="#contribution-to-curriculum">Contribute to Curriculum</a></p>
 
     <h2>Teaching</h2>
-          <p>Student Engagment</p>
-          <p>Adult Learning Strategies</p>
-          <p>Classroom Structure and Layout</p>
+          <p><a href="#student-engagement">Student Engagment</a></p>
+          <p><a href="#adult-learning-strategies">Adult Learning Strategies</a></p>
+          <p><a href="#classroom-structure-and-layout">Classroom Structure and Layout</a></p>
 
       <h2>Professionalism</h2>
-          <p>Economic Collaboratives</p>
-          <p>Continued Learning</p>
-          <p>Ethics and Sensitivity</p>
-          <p>On-Boarding Paperwork (link to docusign coming soon)</p>
-          <p>Operational Responsibipties</p>
+          <p><a href="#economic-collaboratives">Economic Collaboratives</a></p>
+          <p><a href="#continued-learning">Continued Learning</a></p>
+          <p><a href="#ethics-and-sensitivity">Ethics and Sensitivity</a></p>
+          <p><a href="#on-boarding-paperwork">On-Boarding Paperwork (link to docusign coming soon)</a></p>
+          <p><a href="#operational-responsibilities">Operational Responsibipties</a></p>
+          <p><a href="#coaching-and-evaluation">Coaching and Evaluation</a></p>
       <div>
 
-    <div class="left">
-
+    <div class="links">
       <h2>Helpful Links</h2>
         <ul>
           <li>Campus Manager</li>
             <ul>
-              <p>Attendance</li>
-              <li>Grades</li>
-              <li>Screencasts</li>
+              <li><a href="">Attendance</a></li>
+              <li><a href="">Grades</a></li>
+              <li><a href="">Screencasts</a></li>
             </ul>
           <li>Github</li>
             <ul>
               <li>Getting students started with the workbook</li>
                 <ul>
-                  <li>Desktop Application</li>
-                  <li>Terminal</li>
+                  <li><a href="">Desktop Application</a></li>
+                  <li><a href="">Terminal</a></li>
                 </ul>
             </ul>
-          <li>Gitbooks</li>
-          <li>Rocket Chat</li>
-          <li>CircleCi</li>
+          <li><a href="">Gitbooks</a></li>
+          <li><a href="">Rocket Chat</a></li>
+          <li><a href="">CircleCi</a></li>
         </ul>
-
       </div>
 
 
-    <div class="center">
       <h2 id="knowledge-of-students">Knowledge of Students</h2>
           <p>Knowledge of Students Video:Need to make</p>
           <p>Read this <a href="https://mindstepsinc.com/2012/09/activating-prior-knowledge/">Knowledge of Students Article</a></p>
           <p>Complete this assessment:</p>
           <iframe src="https://docs.google.com/a/austincodingacademy.com/forms/d/e/1FAIpQLSe4tujNP3wskr3ac2ZM4W0oGSH5x9PHODIHeqGGTRGCG7gwmA/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-    </div>
 
+      <h2 id="lesson-cycle">Lesson Cycle</h2>
+
+
+      <h2 id="contribution-to-curriculum">Contribution to Curriculum</h2>
+
+
+      <h2 id="student-engagement">Student Engagement</h2>
+
+
+      <h2 id="adult-learning-strategies">Adult Learning Strategies</h2>
+
+
+      <h2 id="classroom-structure-and-layout">Classroom Structures and Layout</h2>
+
+
+      <h2 id="economic-collaboratives">Economic Collaboration</h2>
+
+
+      <h2 id="on-boarding-paperwork">On-Boarding Paperwork</h2>
+
+
+      <h2 id="ethics-and-sensitivity">Ethics and Sensitivity</h2>
+
+
+      <h2 id="operational-responsibilities">Operational Responsibilities</h2>
+
+
+      <h2 id="coaching-and-evaluation">Coaching and Evaluation</h2>
 
 
 
