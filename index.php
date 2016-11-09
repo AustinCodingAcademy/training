58b638943e64e37bf088c6029dbba152d4be1174
Chris Lofton <lofton.chris@gmail.com>
2016-11-08 18:48:52 -0600

    oh yeah

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 
 <h1>Instructor Training</h1>
 
-  <div class="center">
-
     <h2>Planning</h2>
           <p><a href="#knowledge-of-students">Knowledge of Students</a></p>
           <p><a href="#lesson-cycle">Lesson Cycle</a></p>
@@ -27,7 +25,6 @@
           <p><a href="#on-boarding-paperwork">On-Boarding Paperwork (link to docusign coming soon)</a></p>
           <p><a href="#operational-responsibilities">Operational Responsibipties</a></p>
           <p><a href="#coaching-and-evaluation">Coaching and Evaluation</a></p>
-      <div>
 
     <div class="links">
       <h2>Helpful Links</h2>
@@ -57,7 +54,8 @@
           <p>Knowledge of Students Video:Need to make</p>
           <p>Read this <a href="https://mindstepsinc.com/2012/09/activating-prior-knowledge/">Knowledge of Students Article</a></p>
           <p>Complete this assessment:</p>
-          <iframe src="https://docs.google.com/a/austincodingacademy.com/forms/d/e/1FAIpQLSe4tujNP3wskr3ac2ZM4W0oGSH5x9PHODIHeqGGTRGCG7gwmA/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+          <iframe src="https://docs.google.com/a/austincodingacademy.com/forms/d/e/1FAIpQLSe4tujNP3wskr3ac2ZM4W0oGSH5x9PHODIHeqGGTRGCG7gwmA/viewform?embedded=true"
+          width="400" height="300" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 
       <h2 id="lesson-cycle">Lesson Cycle</h2>
 
