97530b2ac86e6fea0fa24d906be9695b3a968e4f
Chris Lofton <lofton.chris@gmail.com>
2016-11-08 20:18:57 -0600

    added margin

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <title>Instructor Training</title>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-  <body style="margin: 10px 300px 10px 300px;">
+  <body style="margin: 10px 350px 10px 350px; padding: 10px;">
 
   <h1>Instructor Training</h1>
     <p>During this training, you will work in 3 domains: Planning, Teaching, and Professionalism. Each domain consists of
