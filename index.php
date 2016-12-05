e2a2c6786ee2359a3def6573b5c9996682f5ca13
Chris Lofton <lofton.chris@gmail.com>
2016-12-05 17:29:09 -0600

    changed sub-domains to assets

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
   <h1>Instructor Training</h1>
     <p>During this training, you will work in 3 domains: Planning, Teaching, and Professionalism. Each domain consists of
-      several sub-domains. The subdomains link to their own pages. These pages include links to readings, videos, form
+      several assets. The assets link to their own sections. These sectionst include links to readings, videos, form
       questions, and outside activities. Your progress through the training modules will be tracked and you will receive
       feedback. Reach out to clofton@austincodingacademy.com with any questions.
     </p>
