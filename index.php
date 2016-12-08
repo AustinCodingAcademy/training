dd1864e04366294be213d58f8797c7e75d4d6ddd
Chris Lofton <lofton.chris@gmail.com>
2016-12-08 16:42:14 -0600

    updated

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
     </div>
     <div class="h_iframe">
       <img class="ratio" src="http://placehold.it/8x6/ffffff/ffffff">
-      <iframe src="https://youtu.be/6cmV5C9LtEY" style="border: 0" frameborder="0" scrolling="yes"></iframe>
+      <iframe src="https://www.youtube.com/embed/6cmV5C9LtEY?t=10s" style="border: 0" frameborder="0" scrolling="yes"></iframe>
     </div>
   </div>
   <div class="asset">
